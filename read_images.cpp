74551d3fad7026c5221e6592baf62829a87d15f8
Harrywang18 <1055679009@qq.com>
2022-08-03 14:06:42 +0800

    update imgs path

--- read_images.cpp
+++ read_images.cpp
@@ -1,18 +1,19 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include <iostream>
+
 using namespace cv;
 
 int main(int argc, char* argv[]) {
     
-    std::string path = "/home/harry/workspace/data/frame/1.png";
+    std::string path = "../test_data/1.png";
     Mat img_mat;
     img_mat = imread(path);
     if (!img_mat.data) {
         printf("NO image data\n");
         return -1;
     }
-    imshow("img", img_mat);
+    imshow(path, img_mat);
     waitKey(0);
     return 0;
 }
