5506d2593df989b93093a87d594b012689361e45
Harrywang18 <1055679009@qq.com>
2022-08-03 16:17:55 +0800

    add video play

--- read_images.cpp
+++ read_images.cpp
@@ -27,20 +27,65 @@ int main(int argc, char* argv[]) {
     temp = imread(path);
     cout << temp.size() << endl;
     
-    // generate video
-    const int fps = 50;
-    VideoWriter video("../video_dir/test.avi", CV_FOURCC('M', 'J', 'P', 'G'), fps, temp.size());
-    String pattern = "../test_rendering_data/*.png";
-    vector<String> fn;
-    glob(pattern, fn, false);
-    size_t size = fn.size();
-    sort(fn.begin(), fn.end(), cmp);
-    for(size_t i = 0 ; i < size ; i++){
-        // cout << fn[i] << endl;
-        Mat img = imread(fn[i]);
-        // resize(img, img, temp.size());
-        video << img;
+    // // generate the video
+    // const int fps = 50;
+    // VideoWriter video("../video_dir/test.avi", CV_FOURCC('M', 'J', 'P', 'G'), fps, temp.size());
+    // String pattern = "../test_rendering_data/*.png";
+    // vector<String> fn;
+    // glob(pattern, fn, false);
+    // size_t size = fn.size();
+    // sort(fn.begin(), fn.end(), cmp);
+    // for(size_t i = 0 ; i < size ; i++){
+    //     // cout << fn[i] << endl;
+    //     Mat img = imread(fn[i]);
+    //     // resize(img, img, temp.size());
+    //     video << img;
+    // }
+    // cout << "generate video complete!" << endl;
+
+    /* play the video */
+    String video_show = "video";
+    namedWindow(video_show, CV_WINDOW_NORMAL);
+    VideoCapture capture("../video_dir/test.avi");
+    if(!capture.isOpened()){
+        cout << "open failed" << endl;
+        return -1;
     }
-    cout << "generate video complete!" << endl;
+    
+    long total_frame = capture.get(CV_CAP_PROP_FRAME_COUNT);
+    
+    
+    // set the start frame id
+    long frame_start = 0;
+    capture.set(CV_CAP_PROP_POS_FRAMES, frame_start);
+
+    // set the stop frame id
+    long frame_stop = total_frame - 1;
+
+    double rate = capture.get(CV_CAP_PROP_FPS);
+    bool stop = false;
+    int delay = 1000 / rate;
+
+    long cur_frame = frame_start;
+
+    Mat frame;
+    while(1){
+        if(!capture.read(frame)){
+            cout << "read video failure" << endl;
+            return -1;
+        }
+        imshow("video", frame);
+        int c = waitKey(delay);
+        if(cur_frame >= frame_stop){
+            cur_frame = frame_start;
+            capture.set(CV_CAP_PROP_POS_FRAMES, frame_start);
+        }
+        cur_frame++;
+    }
+    capture.release();
+    waitKey(0);
+    return 0;
+
+
     return 0;
 }
